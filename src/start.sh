85a24640dd7677ed9294900db1e6de6daf729928
Daniel Freytag <daniel@frytg.com>
2020-07-19 15:46:40 +0200

    removing log of nginx file

--- src/start.sh
+++ src/start.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
-envsubst '$DOMAIN' < $PWD/nginx.template > $PWD/nginx.conf && cat $PWD/nginx.conf
+envsubst '$DOMAIN' < $PWD/nginx.template > $PWD/nginx.conf
 
 nginx -c "$PWD/nginx.conf"
 
